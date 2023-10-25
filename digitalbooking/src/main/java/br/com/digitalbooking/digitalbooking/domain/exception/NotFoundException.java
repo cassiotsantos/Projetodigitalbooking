c04015dd70dcbd072d88ed1d0c11b2e9fb7189a9
cassiotsantos <psyteqkilla@gmail.com>
2023-10-25 19:59:36 -0300

    add crud Delete e Update - Inclusão de Expection

---
+++ digitalbooking/src/main/java/br/com/digitalbooking/digitalbooking/domain/exception/NotFoundException.java
@@ -0,0 +1,11 @@
+package br.com.digitalbooking.digitalbooking.domain.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.UUID;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(UUID id) {super("ID: %s não encontrado".formatted(id));}
+}
