30aab165ea9b789c6c069ee003aba3c0519fe443
cassiotsantos <psyteqkilla@gmail.com>
2023-11-11 12:48:10 -0300

    ajuste nas categorias

--- digitalbooking/src/main/java/br/com/digitalbooking/digitalbooking/DigitalbookingApplication.java
+++ digitalbooking/src/main/java/br/com/digitalbooking/digitalbooking/DigitalbookingApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DigitalbookingApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DigitalbookingApplication.class, args);
 	}
--- digitalbooking/src/main/java/br/com/digitalbooking/digitalbooking/domain/repository/CategoriasRepository.java
+++ digitalbooking/src/main/java/br/com/digitalbooking/digitalbooking/domain/repository/CategoriasRepository.java
@@ -9,5 +9,6 @@ import java.util.UUID;
 
 @Repository
 public interface CategoriasRepository extends JpaRepository<Categorias, UUID> {
+
   List<Categorias> findByNomeStartingWith(String termo);
 }
