d08471bc359ea9e85e419bb2dd7e6a7dd2ec7cde
cassiotsantos <psyteqkilla@gmail.com>
2023-12-03 23:58:54 -0300

    update no controller de utenticação 2 (pendente register )

--- digitalbooking/src/main/java/br/com/digitalbooking/digitalbooking/api/controller/AuthenticationController.java
+++ digitalbooking/src/main/java/br/com/digitalbooking/digitalbooking/api/controller/AuthenticationController.java
@@ -1,12 +1,10 @@
 package br.com.digitalbooking.digitalbooking.api.controller;
 
-import br.com.digitalbooking.digitalbooking.api.dto.request.AuthenticationDTO;
 import br.com.digitalbooking.digitalbooking.api.dto.request.AuthenticationSingInDTO;
 import br.com.digitalbooking.digitalbooking.api.dto.request.AuthenticationSingUpDTO;
 import br.com.digitalbooking.digitalbooking.api.dto.response.AuthenticationResponseDTO;
 import br.com.digitalbooking.digitalbooking.domain.entity.SingIn;
 import br.com.digitalbooking.digitalbooking.domain.entity.SingUp;
-import br.com.digitalbooking.digitalbooking.domain.entity.Usuarios;
 import br.com.digitalbooking.digitalbooking.domain.repository.UserRepository;
 import br.com.digitalbooking.digitalbooking.domain.repository.UsuariosRepository;
 import br.com.digitalbooking.digitalbooking.domain.service.AuthenticationUserService;
@@ -16,7 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,8 +22,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("v1/authentication")
 public class AuthenticationController {
-
-    private final AuthenticationUserService authenticationUserService;
+    @Autowired
+    private AuthenticationUserService authenticationUserService;
     @Autowired
     private AuthenticationManager authenticationManager;
     @Autowired
@@ -35,14 +32,6 @@ public class AuthenticationController {
     private UserRepository userRepository;
     @Autowired
     private JwtUtil jwtUtil;
-
-    public AuthenticationController(AuthenticationUserService authenticationUserService, AuthenticationManager authenticationManager, UsuariosRepository repository, UserRepository userRepository, JwtUtil jwtUtil) {
-        this.authenticationUserService = authenticationUserService;
-        this.authenticationManager = authenticationManager;
-        this.repository = repository;
-        this.userRepository = userRepository;
-        this.jwtUtil = jwtUtil;
-    }
 
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody @Valid AuthenticationSingInDTO request){
