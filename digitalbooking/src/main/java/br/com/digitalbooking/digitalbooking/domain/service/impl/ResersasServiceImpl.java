3618c57b3d65965516046a8f564f31bb2afa7bb6
Cássio Teixeira <psyteqkilla@gmail.com>
2023-12-04 22:17:29 -0300

    update class ResersasServiceImpl

--- digitalbooking/src/main/java/br/com/digitalbooking/digitalbooking/domain/service/impl/ResersasServiceImpl.java
+++ digitalbooking/src/main/java/br/com/digitalbooking/digitalbooking/domain/service/impl/ResersasServiceImpl.java
@@ -2,10 +2,15 @@ package br.com.digitalbooking.digitalbooking.domain.service.impl;
 
 import br.com.digitalbooking.digitalbooking.domain.entity.Reservas;
 import br.com.digitalbooking.digitalbooking.domain.service.ReservasService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
 
+
+@Service
+@AllArgsConstructor
 public class ResersasServiceImpl implements ReservasService {
     @Override
     public Reservas criarReserva(Reservas reservas) {
